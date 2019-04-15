5682a201f0b3a6217330a767f62b38108e56aa1e
sserges <agounkeserge@gmail.com>
2019-04-15 17:24:34 +0000

    Video-list and video-list-item

--- src/containers/app.js
+++ src/containers/app.js
@@ -20,21 +20,30 @@ class App extends Component {
   }
 
   componentWillMount() {
+    this.initMovies();
+  }
+
+  initMovies() {
     axios
       .get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`)
       .then(response => {
-        console.log(response);
-        this.setState({ movieList: response.data.results.slice(1, 6) });
-        this.setState({ currentMovie: response.data.results[0] });
-        console.log(this.state);
+        this.setState({
+          movieList: response.data.results.slice(1, 6),
+          currentMovie: response.data.results[0]
+        });
       });
   }
 
   render() {
+    const renderVideoList = () => {
+      if (this.state.movieList.length >= 5) {
+        return <VideoList movieList={this.state.movieList} />;
+      }
+    };
     return (
       <div>
         <SearchBar />
-        <VideoList />
+        {renderVideoList()}
         <VideoDetail
           title={this.state.currentMovie.title}
           description={this.state.currentMovie.overview}
--- src/containers/video-list.js
+++ src/containers/video-list.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import VideoListItem from "../components/video-list-item";
 
-const VideoList = () => {
-  const movies = ["film1", "film2", "film3", "film4", "film5"];
+const VideoList = ({ movieList }) => {
+  console.log(movieList);
+
   return (
     <div>
       <ul>
-        {movies.map((movie, index) => {
-          return <VideoListItem key={index} movie={movie} />;
+        {movieList.map(movie => {
+          return <VideoListItem key={movie.id} movie={movie} />;
         })}
       </ul>
     </div>
